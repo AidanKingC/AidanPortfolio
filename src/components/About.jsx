25ef747df846d74001f79f6bbd324d67c0c37f0d
AidanKingC <aidankingc@gmail.com>
2023-11-10 21:04:31 -0600

    fixed images hopefully

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
       <div  className="shadow-lg flex flex-col items-center md:flex-row">
           <img
             className="block object-cover md:h-full md:w-1/2"
-            src="https://lh3.googleusercontent.com/pw/ADCreHcpOXwqPbumBI0KmYvBNTzggBykd4asgYjH3vCpoWwufZEw1qInKLoPZo-iNv6uw2kUFyM_vSCRnsNUYJfC5z3w6AsE87hqwobSGJ5VlNg9VVPbOqjHuGMTTl4s7gkfbNJXqkuPP4eLVvxVMSqyRCdLyzfgAwQgkEFplkIp-hZT6XnLJxs7b7JQYcstL1De-OPq5yzJ7h1rc8doqs4HTJa08jjNhiSZ062nGY75UiCaJfT8oMxqArmdWFG2525evmYARWzI49x4Nxq3SUsHYSn-WqWVG3uTatrVKX7r-BeP0STMII_R-HJYyi4oukm9U2d46xd7GxqdmhMz4oM78bqrogSGAF5GKz6PmAHIpPVEVcMDsMs0mYclNMDAVoySX8Uj0O2wJsWYwOUUIFJ_oOHKwzYUGOYolortnIShcrJNSDRa3lhKBvfdvGLZtoIb8ulZzQnzclfb4rpt4AH8b0VkF9xRZBhfdMf9emLDHCNXsSMV3MNxM5ckpb8dvAjbHhjljAsvnrYTwgHo4cSxMgnB68pkQUOcTJWrMjL_-4TfZnJnyejnD-pHuK88NLDe1StYFUSNlDQhl1g7m0fOdaqf1VvuJrCtjgr2RjLFM2GkI3a7I8TLIdaHZN7lOuCOBjuoVYU9DhlFvAdZtGKVsxztXFqSz0IzCtXosZS3eKWOsaEcFJQUNy-3XHWmWHgdfujZD9MEiAM8QUx5SH4Ykw8j-93fbPtadG__a2xebJhreX9JQ19GVm8m4E1r0thvYltwuaR5CUy8Fdp8x2X0DQY_dhp1BXI_7nWkz_k7uQbxxc3H5y2mppb-nzVtsuRHldBPJMKeqni8i5tZeaWJquLmzBd_PEO7yOsO28gOJwbvM2rDnExkxhTHTKBQjNJ2q4mzYn2ccGXkQ8PGgX2P26SG0DH4mooJ7hCe7-zQpmDcUkOQjbbZN7rNw8wb=w919-h919-s-no?authuser=0"
+            src="AboutPic.jpg"
           />
         <div className="h-full flex flex-col justify-left items-center md:items-start p-3">
           <h1 className="text-3xl">
